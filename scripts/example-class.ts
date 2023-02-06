d3dcc6630f936b5f594e3264e2530c97d0c303e9
vbit32 <valerie.bay2@gmail.com>
2023-02-05 21:44:33 -0500

    added more logic and comments for following through program

--- scripts/example-class.ts
+++ scripts/example-class.ts
@@ -1,3 +1,13 @@
+//the following class instantiates an object with the given passed paramters, and then determines what the payment due date is.
+//the program starts with the due date being set to the given pay date
+//it then calculates if the current due date falls on a weekend
+//if so, it will determine whether or not to increment or decrement by 1 day depending on if it is a sunday or a saturday
+//it then calculates if the current due date is a holiday
+//if so, it will always always decrement
+//it will then check to see if it has been at least 10 days since the fund day
+//if it has been less than 10 days, it will defer to the next pay date, either adding 7, 14, or 30 days depending on the pay span
+//the program will loop recursively until a value is returned
+
 class PayDateCalculator {
 
     //The day the loan was funded
@@ -40,7 +50,6 @@ class PayDateCalculator {
         if(this.hasDirectDeposit == true)
         {
             this.calculateWeekend();
-
         }
         //else direct deposit is not enabled
         else 
@@ -50,49 +59,64 @@ class PayDateCalculator {
             this.calculateWeekend();
         }
             
-            return this.dueDate;
+        return this.dueDate;
     }
 
         //step 2 in the calculation, after we check if the object has direct deposite enabled or not
-    public calculateWeekend(): Date{
+    public calculateWeekend(): void{
         if(this.isWeekend() == true)
         {
-            //check if its a saturday or a sunday
-            if (this.isMondayAHoliday() == true && this.isSunday() == true && this.tenDaysSinceFundDay() == true)
+            //logic for case when current due date value is: a sunday + the following monday also lands on a holiday + its been at least ten days funding
+            // - a sunday
+            // - the following monday also lands on a holiday
+            // - its been at least ten days since the funding date
+            // in this case, pay date decrements by 1
+            if (this.isSunday() == true)
             {
-
+                this.addDay();
+                this.calculateHoliday();
             }
-            else if (this.isMondayAHoliday() == true && this.isSaturday() == true && this.tenDaysSinceFundDay() == true && daysSinceFundDay > 10){
-
+            else if (this.isSaturday() == true)
+            {
+                this.subtractDay();
+                this.calculateHoliday();
             }
         }
         else 
         {
-            if (this.isHoliday() == true)
-            {
+            this.calculateHoliday();
+        }
+    }
 
-                //add logic to check if the holiday is on a monday
-                this.dueDate.setDate(this.dueDate.getDate() - 1);
-                this.calculateDueDate();
+    public calculateHoliday(): void {
+        if (this.isHoliday() == true)
+        {
+            //check if the current holiday due date value is a monday
+            //if yes, increment
+            //if no, decrement
+            if (this.dueDate.getDay() == 1)
+            {
+                this.addDay //incremenet
+                this.calculateWeekend(); //and then check again if its a weekend
             }
-            else //if its not a holiday
-            {
-                //check if its been at least 10 days since the fund day
-                if (this.tenDaysSinceFundDay() == true)
-                {
-                    return this.dueDate;
-                }
-                else 
-                {
-
-                }
+            else {
+                this.subtractDay(); //not a monday, so decrement
+                this.calculateWeekend(); //reverse loop, and go back to check if its a weekend
             }
         }
-        return this.dueDate;
+        else {
+            //not a holiday or a weekend, go to the final check
+            this.calculateFundDay();
+        }
+
+    }
+
+    public calculateFundDay():void {
+
     }
 
     public isWeekend(): boolean {
-        var dayOfWeek = this.payDay.getDay(); // get numeric day of week, Sunday - Saturday : 0 - 6
+        var dayOfWeek = this.dueDate.getDay(); // get numeric day of week, Sunday - Saturday : 0 - 6
         var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 0); // 6 = Saturday, 0 = Sunday
         return isWeekend;
     }
@@ -100,7 +124,7 @@ class PayDateCalculator {
     public isHoliday():boolean {
         for (let i=0 ; i<this.holidays.length ; i++)
         {
-            if (this.payDay == this.holidays[i].holidayDate)
+            if (this.dueDate == this.holidays[i].holidayDate)
             {
                 return true;
             }
@@ -109,9 +133,12 @@ class PayDateCalculator {
     }
 
     //checks if the next up coming holiday lands on a monday
-    //if it does
     public isMondayAHoliday(): boolean {
-        if (this.dueDate.getDay() == 0)
+        if (this.isSunday() == true)
+        {
+
+        }
+        else if (this.isSaturday() == true)
         {
 
         }
@@ -147,6 +174,12 @@ class PayDateCalculator {
     }
 }
 
+//the following class instantiates an object with the given passed paramters, and then determines what the payment due date is.
+//the program starts with the due date being set to the given pay date
+//it then calculates if the current due date falls on a weekend
+//if so, it will determine whether or not to increment or decrement by 1 day
+//it then calculates if the current due date is a holiday
+//if so, it will always always decrement UNLESS the given holiday falls on a monday, only then will it increment.
 
 //TEST OBJECTS update these later
 //let mercObj = new PayDateCalculator("Mercedes-Benz GLA");
