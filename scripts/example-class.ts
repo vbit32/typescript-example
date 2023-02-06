ecda0624a406215713b56e0b09cdcfbcda5d6081
vbit32 <valerie.bay2@gmail.com>
2023-02-05 19:25:49 -0500

    expanded upon code and added more class functionality

--- scripts/example-class.ts
+++ scripts/example-class.ts
@@ -14,6 +14,8 @@ class PayDateCalculator {
     // customer receives their paycheck via direct deposit.
     hasDirectDeposit: boolean;
 
+    dueDate: Date;
+
     //multi dimensional array of dates containing holidays, each index containing holidayName and holidayDate
     holidays = [
         { holidayName: "New Years Day", holidayDate: new Date("January 1 2023") },
@@ -23,29 +25,79 @@ class PayDateCalculator {
       ]
 
     //this is where the object is instantiated
-    constructor(fundDay:Date, payDay: Date, paySpan: string, hasDD: boolean) {
+    constructor(fundDay: Date, payDay: Date, paySpan: string, hasDD: boolean) {
         this.fundDay = fundDay;
         this.payDay = payDay;
         this.paySpan = paySpan;
         this.hasDirectDeposit = hasDD;
+        //calculate dueDate later
+        this.dueDate = this.payDay;
     }
     
+    public calculateDueDate(): Date {
+       // this.dueDate = this.payDay;
+        if(this.hasDirectDeposit == true)
+        {
+            if(this.isWeekend() == true)
+            {
+                //check if its a saturday or a sunday
 
-    public calculateDueDate(fundDay: Date, paySpan: string,
-        payDay: Date, hasDirectDeposit: boolean): Date {
-        
-        
-        //add logic
-        return dueDate;
+            }
+            else 
+            {
+                if (this.isHoliday() == true)
+                {
+
+                    //add logic to check if the holiday is on a monday
+                    this.dueDate.setDate(this.dueDate.getDate() - 1);
+                    this.calculateDueDate();
+                }
+                else //if its not a holiday
+                {
+                    //check if its been at least 10 days since the fund day
+                    if (this.tenDaysSinceFundDay() == true)
+                    {
+
+                    }
+                    else 
+                    {
+                        
+                    }
+                }
+            }
+        }
+        else 
+        {
+
+        }
+
+        //just filler return for now
+        return new Date ("January 12 2023");
+        }
+
+    public isWeekend(): boolean {
+        var dayOfWeek = this.payDay.getDay(); // get numeric day of week, Sunday - Saturday : 0 - 6
+        var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 0); // 6 = Saturday, 0 = Sunday
+        return isWeekend;
+    }
+
+    public isHoliday():boolean {
+        for (let i=0 ; i<this.holidays.length ; i++)
+        {
+            if (this.payDay == this.holidays[i].holidayDate)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public tenDaysSinceFundDay():boolean {
+        //returns true or false depending on if the due date is greater than or equal to 10 days mroe than the fund day
+        return this.dueDate.getDate() >= (this.fundDay.getDate()+10); 
     }
 }
-
-
 
 
 //TEST OBJECTS update these later
 //let mercObj = new PayDateCalculator("Mercedes-Benz GLA");
 //let hondaObj = new PayDateCalculator("Honda City")
-
-//mercObj.run();  // A Mercedes started A Mercedes-Benz GLA is moving at 150 mph!
-//hondaObj.run(); // A Honda started A Honda City is moving at 100 mph!
